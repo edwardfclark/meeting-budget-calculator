b735e58b38400c25efa7b323640f874a729379e7
Edward Clark <edward.clark@usaycompare.com>
2019-10-26 17:36:04 +0100

    Add lodash

--- src/components/Participants/participants.js
+++ src/components/Participants/participants.js
@@ -4,7 +4,6 @@ import { Typography } from "@material-ui/core";
 
 const Participants = () => {
   const { participants } = React.useContext(MeetingContext);
-  console.log(participants);
   return <Typography variant="body1">Ayyy lmao</Typography>;
 };
 
